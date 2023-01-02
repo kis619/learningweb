0bd29383be82bc911e0dc8ff2a52eee3ec590c79
kmilchev <kmilchev@student.42wolfsburg.de>
2023-01-02 17:47:31 +0100

    yes

---
+++ events/scripts/selecting-thingies.js
@@ -0,0 +1,11 @@
+var header = document.getElementsByTagName("header");
+console.log(header);
+
+var checklist = document.getElementById("lyrics-list");
+console.log(checklist);
+
+var listItems = checklist.querySelectorAll("li");
+console.log(listItems);
+
+var h1 = header.querySelector("h1");
+// console.log(h1);
